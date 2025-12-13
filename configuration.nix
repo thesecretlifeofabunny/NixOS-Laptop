9623e293d2830e20fdbb3e37af841677194e4a16
thesecretlifeofabunny <212422141+thesecretlifeofabunny@users.noreply.github.com>
2025-12-13 00:17:11 -0800

    docker addition

--- configuration.nix
+++ configuration.nix
@@ -95,7 +95,21 @@
     nerd-fonts.droid-sans-mono
     font-awesome
   ];
-  
+
+  virtualisation.docker = {
+    enable = true;
+
+    rootless = {
+      enable = true;
+      setSocketVariable = true;
+
+      daemon.settings = {
+        dns = [ "1.1.1.1" "8.8.8.8" ];
+        registry-mirrors = [ "https://mirror.gcr.io" ];
+      };
+    };
+  };
+
   nixpkgs.config.allowUnfree = true; 
   
   environment.systemPackages = with pkgs; [
